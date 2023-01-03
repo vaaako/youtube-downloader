cef1f7782632ddeef30d04bcc2d89b5023270749
vako <66805221+vaaako@users.noreply.github.com>
2023-01-03 02:26:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,27 +7,37 @@ def convert_to_mp3(filename):
 	clip.audio.write_audiofile(filename[:-4] + ".mp3")
 	clip.close()
 
-def main():
-	link = input("Enter the link: ")
+def download(link, video):
 	yt = YouTube(link.strip())
-	# yt = YouTube("https://www.youtube.com/watch?v=Ba463jCxmow")
-
 
 	print("\nTitle: "+yt.title)
 	print("Thumbanil: "+yt.thumbnail_url)
 
-	stream = yt.streams.filter().get_by_itag(22)
-	stream.download('downloads/')
-	convert_to_mp3('downloads/'+stream.default_filename)
-	os.remove('downloads/'+stream.default_filename)
+	# stream = yt.streams.filter().get_by_itag(22)
+	stream = yt.streams.get_highest_resolution()
+	stream.download('downloads/') # Download video
+
+	if video != 'n':
+		convert_to_mp3('downloads/'+stream.default_filename)
+		os.remove('downloads/'+stream.default_filename)
+
+def main():
+	link = input("Enter the link: ")
+	video = input("Download as audio? (Y/n): ").lower()
+
+	download(link, video)
+	print("Finished!")
+
 
 if __name__ == '__main__':
 	main()
 
-# Quality | itag
-# Low (360p) = 18
-# Medium = (720p) 22
-# High (1080p) = 137
-# Very High (2160p) = 313
+"""
+QUALITY           | ITAG
+Low (360p)        - 18
+Medium (720p)     - 22
+High (1080p)      - 137
+Very High (2160p) - 313
 
-# 140 = Audio only
+140 = Audio only (bad quality)
+"""
